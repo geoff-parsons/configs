7cfa192c39e385b2fe24ee3319b0afb2e4e1fd17
Geoff Parsons <geoffrey.parsons@gmail.com>
2009-05-21 16:09:13 -0400

    Added scripts which will be copied over as well. - webkit2png from Paul Hammond - battery status - reordered copy_configs script

--- copy_configs.rb
+++ copy_configs.rb
@@ -4,14 +4,6 @@ require 'etc'
 home_dir = Etc.getpwuid.dir
 dir = File.dirname(__FILE__)
 
-# zsh
-if `which mate`.length == 0
-  puts "You appear to not have TextMate installed or don't have the shell script installed with it:"
-  puts "  Some settings in ~/.zshrc will not work."
-end
-FileUtils.copy(dir + '/zsh', home_dir + '/.zshrc')
-FileUtils.ln_s(home_dir + '/.zshrc', home_dir + '/.zshenv', :force => true)
-
 # Ruby
 if `which gem`.length == 0
   puts "You appear to not have ruby gems installed:"
@@ -31,3 +23,18 @@ system('git config --global web.browser lynx') if `which lynx`.length > 0
 # Subversion
 FileUtils.mkdir(home_dir + '/.subversion') unless File.exists?(home_dir + '/.subversion')
 FileUtils.copy(dir + '/subversion', home_dir + '/.subversion/config')
+
+# Scripts
+FileUtils.mkdir(home_dir + '/bin') unless File.exists?(home_dir + '/bin')
+FileUtils.copy(dir + '/scripts/battery-status.rb', home_dir + '/bin/battery')
+FileUtils.chmod(0755, home_dir + '/bin/battery')
+FileUtils.copy(dir + '/scripts/webkit2png.py', home_dir + '/bin/webkit2png')
+FileUtils.chmod(0755, home_dir + '/bin/webkit2png')
+
+# zsh
+if `which mate`.length == 0
+  puts "You appear to not have TextMate installed or don't have the shell script installed with it:"
+  puts "  Some settings in ~/.zshrc will not work."
+end
+FileUtils.copy(dir + '/zsh', home_dir + '/.zshrc')
+FileUtils.ln_s(home_dir + '/.zshrc', home_dir + '/.zshenv', :force => true)
