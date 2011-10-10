f1fae81046d95fd22f54d89c5943ff2806b18282
Geoff Parsons <geoffrey.parsons@gmail.com>
2011-10-10 09:45:09 -0400

    Rearranged files.

--- copy_configs.rb
+++ copy_configs.rb
@@ -2,7 +2,9 @@ require 'fileutils'
 require 'etc' 
 
 home_dir = Etc.getpwuid.dir
-dir = File.dirname(__FILE__)
+config_dir = File.join( File.dirname(__FILE__), 'config' )
+script_dir = File.join( File.dirname(__FILE__), 'scripts')
+
 os = case `uname`
        when /linux/i
          :linux
@@ -20,18 +22,18 @@ if `which gem`.length == 0
   puts "You appear to not have ruby gems installed:"
   puts "  IRB config will not work without removing the line \"require 'rubygems'\""
 else
-  FileUtils.copy(dir + '/ruby_gems', home_dir + '/.gemrc')
+  FileUtils.copy( File.join(config_dir, 'ruby_gems'), File.join(home_dir, '.gemrc') )
 end
-FileUtils.copy(dir + '/irb', home_dir + '/.irbrc')
-FileUtils.copy(dir + '/autotest', home_dir + '/.autotest')
+FileUtils.copy( File.join(config_dir, 'irb'), File.join(home_dir, '.irbrc') )
+FileUtils.copy( File.join(config_dir, 'autotest'), File.join(home_dir, '.autotest') )
 
 
 ##
 ## Git
 ##
 
-FileUtils.copy(dir + '/git', home_dir + '/.gitconfig')
-FileUtils.copy(dir + '/git_ignores', home_dir + '/.gitignore')
+FileUtils.copy( File.join(config_dir, 'git'), File.join(home_dir, '.gitconfig') )
+FileUtils.copy( File.join(config_dir, 'git_ignores'), File.join(home_dir, '.gitignore') )
 # see if we have lynx, if so set it as the default web browser for git help --web
 system('git config --global web.browser lynx') if `which lynx`.length > 0
 
@@ -40,8 +42,10 @@ system('git config --global web.browser lynx') if `which lynx`.length > 0
 ## Subversion
 ##
 
-FileUtils.mkdir(home_dir + '/.subversion') unless File.exists?(home_dir + '/.subversion')
-FileUtils.copy(dir + '/subversion', home_dir + '/.subversion/config')
+unless File.exists?( File.join(home_dir, '.subversion') )
+  FileUtils.mkdir( File.join(home_dir, '.subversion') ) 
+end
+FileUtils.copy( File.join(config_dir, 'subversion'), File.join(home_dir, '.subversion/config') )
 
 
 ##
@@ -50,30 +54,34 @@ FileUtils.copy(dir + '/subversion', home_dir + '/.subversion/config')
 
 FileUtils.mkdir(home_dir + '/bin') unless File.exists?(home_dir + '/bin')
 
-FileUtils.copy(dir + '/scripts/battery-status.rb', home_dir + '/bin/battery')
-FileUtils.chmod(0755, home_dir + '/bin/battery')
+FileUtils.copy( File.join(script_dir, 'battery-status.rb'), File.join(home_dir, 'bin', 'battery') )
+FileUtils.chmod(0755, File.join(home_dir, 'bin', 'battery') )
 
 if os == :OSX
-  FileUtils.copy(dir + '/scripts/webkit2png.py', home_dir + '/bin/webkit2png')
-  FileUtils.chmod(0755, home_dir + '/bin/webkit2png')
+  FileUtils.copy( File.join(script_dir, 'webkit2png.py'), File.join(home_dir, 'bin', 'webkit2png') )
+  FileUtils.chmod(0755, File.join(home_dir, 'bin', 'webkit2png') )
 end
 
-FileUtils.copy(dir + '/scripts/tmux-rails.sh', home_dir + '/bin/tmux-rails')
-FileUtils.chmod(0755, home_dir + '/bin/tmux-rails')
+FileUtils.copy( File.join(script_dir, 'tmux-rails.sh'), File.join(home_dir, 'bin', 'tmux-rails') )
+FileUtils.chmod(0755, File.join(home_dir, 'bin', 'tmux-rails') )
+
+FileUtils.copy( File.join(script_dir, 'ssh-public-key.sh'), File.join(home_dir, 'bin', 'ssh-public-key') )
+FileUtils.chmod(0755, File.join(home_dir, 'bin', 'ssh-public-key') )
+
 
 
 ##
 ## tmux
 ##
 
-FileUtils.copy(dir + '/tmux', home_dir + '/.tmux.conf')
+FileUtils.copy( File.join(config_dir, 'tmux'), File.join(home_dir, '.tmux.conf') )
 
 
 ##
 ## zsh
 ##
 
-FileUtils.copy(dir + '/zsh', home_dir + '/.zshrc')
+FileUtils.copy( File.join(config_dir, 'zsh'), File.join(home_dir, '.zshrc') )
 FileUtils.ln_s(home_dir + '/.zshrc', home_dir + '/.zshenv', :force => true)
 
 
