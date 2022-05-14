2169d38cec3d53b4236a50b36682df14d4fdce99
Geoff Parsons <geoffrey.parsons@gmail.com>
2022-05-14 16:31:03 -0700

    Add Object#method_grep to irb config for searching an object's methods for a pattern.

--- config/irb.rb
+++ config/irb.rb
@@ -9,8 +9,8 @@ IRB.conf[:PROMPT_MODE]  = :SIMPLE
 IRB.conf[:AUTO_INDENT]  = true
 IRB.conf[:USE_READLINE] = true
 
+##
 # Quick benchmarking
-# Based on rue's irbrc => http://pastie.org/179534
 def benchmark(repetitions=100, &block)
   require 'benchmark'
   Benchmark.bmbm do |b|
@@ -18,11 +18,26 @@ def benchmark(repetitions=100, &block)
   end
 end
 
+##
 # Provides RI lookup for the object given.
-# ri(Array) = ri("Array") = ri([1,2,3])
+# @example
+#   ri(Array)
+#   ri("Array")
+#   ri([1,2,3])
 def ri(obj = '')
   unless obj.kind_of?(Class) or obj.kind_of?(String)
     obj = obj.class
   end
   puts `ri #{obj}`
+end
+
+class Object
+  ##
+  # Search an object's method for something matching a pattern.
+  # @example
+  #   [1,2,3].method_grep(/sort/) # => [:sort, :sort!, :sort_by, :sort_by!]
+  #   Math.method_grep('cos') # => [:acos, :acosh, :cos, :cosh]
+  def method_grep(search)
+    self.methods.sort.find_all { |method| method.to_s.match(search) }
+  end
 end
